d616d3d3514dcfc5d9812e3ae84ad3a69c7569d3
Melody <machineonamission@gmail.com>
2026-03-24 18:34:07 -0500

    guga

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 git pull
 #sleep 1
 #systemctl stop smarthome-pod.service
-sudo -u melody cp -r ./pipewire/* /home/melody/.config/pipewire/pipewire.conf.d/
+#sudo -u melody cp -r ./pipewire/* /home/melody/.config/pipewire/pipewire.conf.d/
 sudo find ./quadlet -type f -exec cp {} /etc/containers/systemd/ \;
 sleep 2
 sudo systemctl daemon-reload
